62682b752e1fdf1492a2c34d7d59ebc4660b2dee
pangoleaf <mascot26@tuta.io>
2021-11-26 14:57:37 +0000

    initial commit of base project as generated

---
+++ src/main/java/com/pangoleaf/obra/OneBeatRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.pangoleaf.obra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneBeatRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneBeatRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pangoleaf/obra/OneBeatRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pangoleaf.obra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneBeatRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
